2e753cc46239c78737ceef2955b0a73bfee931d4
Andy <mazhiyuannn@gmail.com>
2023-09-28 13:00:35 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,50 @@
+import java.util.Arrays;
+import java.io.File;
+
 public class Driver {
 	public static void main(String [] args) {
 		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		if ((p.coefficients == null) || (p.exponents == null))
+		{
+			System.out.println("Bruh this polynomial empty");
+		}
+		else{
+			System.out.println(p.evaluate(3));
+		}
+		double [] c1 = {5, 6, 4};
+		int [] e1 = {3, 0, 2};
+		Polynomial p1 = new Polynomial(c1, e1);
+		double [] c2 = {-2, -9, 3};
+		int [] e2 = {1, 4, 7};
+		Polynomial p2 = new Polynomial(c2, e2);
 		Polynomial s = p1.add(p2);
+		System.out.println(Arrays.toString(s.coefficients));
+		System.out.println(Arrays.toString(s.exponents));
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
 		if(s.hasRoot(1))
 		System.out.println("1 is a root of s");
 		else
 		System.out.println("1 is not a root of s");
+
+		//test multiply
+		Polynomial productPolynomial = p1.multiply(p2);
+		System.out.println("Here is where we multiply our two polynomials");
+		System.out.println(Arrays.toString(productPolynomial.coefficients));
+		System.out.println(Arrays.toString(productPolynomial.exponents));
+
+		//test file constructor
+		System.out.println("Here is where we print out the polynomial from a file");
+		File text = new File("C:\\Users\\mazhi\\OneDrive\\Documents\\GitHub\\b07lab1\\textPolynomial.txt");
+		Polynomial polynomial_fromfile = new Polynomial(text);
+		System.out.println("Here is the polynomial from the file:");
+		System.out.println(Arrays.toString(polynomial_fromfile.coefficients));
+		System.out.println(Arrays.toString(polynomial_fromfile.exponents));
+
+		//test file writer
+		double[] test_coefficents = new double[] {4, -2, 5};
+		int[] test_exponents = new int[] {2, 5, 0};
+		Polynomial somePolynomial = new Polynomial(test_coefficents, test_exponents);
+		System.out.println("Here is where we save our polynomial to a file");
+		somePolynomial.saveToFile("newPolynomial");
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,43 +1,224 @@
 import java.util.Arrays;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Polynomial {
 	double[] coefficients;
 	int[] exponents;
 
 	public Polynomial() {
-		coefficients = new double[] {0};
-		exponents = new int[] {0};
+		coefficients = null;
+		exponents = null;
 	}
 
 	public Polynomial(double[] inputCoefficients, int[] inputExponents) {
-		coefficients = inputCoefficients.clone();
-		exponents = inputExponents.clone();
+		//only clone if not null
+		if (inputCoefficients != null){
+			coefficients = inputCoefficients.clone();
+		}
+		else {
+			coefficients = null;
+		}
+		
+		//same for exponents
+		if (inputExponents != null){
+			exponents = inputExponents.clone();
+		}
+		else {
+			exponents = null;
+		}
 	}
-	
-	public Polynomial add(Polynomial another_polynomial) {
-		int length1 = coefficients.length;
-		int length2 = another_polynomial.coefficients.length;
-		
-		//take the minimum of the 2 lengths cuz we only need the shorter one to run 
-		//the for loop
-		int shorter_length = Math.min(length1, length2);
-		
-		Polynomial sum_polynomial = new Polynomial();
 
-		if (length1 == shorter_length)
-		{
-			sum_polynomial = new Polynomial(another_polynomial.coefficients, another_polynomial.exponents);
+	//helper function to determine if a value is a coefficient or not within a polynomial array
+	public boolean isCoefficient(String[] polynomial_Array, int index_of_value){
+		//middle cases
+		if ((index_of_value + 1 < polynomial_Array.length) && (index_of_value - 1 >= 0)){
+			if (!polynomial_Array[index_of_value - 1].equals("x")) {
+				if ( polynomial_Array[index_of_value + 1].equals("x") ||
+				polynomial_Array[index_of_value + 1].equals("-") || 
+				polynomial_Array[index_of_value + 1].equals("+")){
+					return true;
+				}
+			}
 		}
-		else
-		{
-			sum_polynomial = new Polynomial(coefficients, exponents);
+		//first index
+		if (index_of_value == 0){
+			if (polynomial_Array[index_of_value + 1].equals("+") || 
+			polynomial_Array[index_of_value + 1].equals("-") || 
+			polynomial_Array[index_of_value + 1].equals("x")){
+				return true;
+			}
 		}
-		
-		for (int i = 0; i < shorter_length; i++){
-			sum_polynomial.coefficients[i] = coefficients[i] + another_polynomial.coefficients[i];
+		//last index
+		if(index_of_value == polynomial_Array.length - 1){
+			if ((polynomial_Array[index_of_value - 1].equals("+")) ||
+			(polynomial_Array[index_of_value - 1].equals("-"))){
+				return true;
+			}
 		}
-		
-		return sum_polynomial;
+		return false;
+	}	
+
+	//helper function to determine if a value is an exponent or not within a polynomial array
+	public boolean isExponent(String[] polynomial_Array, int index_of_value){
+
+		//make sure we aint out of bounds
+		if (index_of_value <= 0 || index_of_value >= polynomial_Array.length){
+			return false;
+		}
+		if ( polynomial_Array[index_of_value - 1].equals("x")){
+			return true;
+		}
+		return false;
+	}
+
+
+	//helper function to remove an element from an array but for doubles
+	public double[] removeElement(double[] arr, int remove_index){
+		double[] newArr = new double[arr.length - 1];
+		int length_to_index = 0;
+        //for loop to find the length to the index to be removed
+        for (int i = 0; i < arr.length; i++) {
+            if (length_to_index == remove_index){
+                break;
+            }
+			length_to_index++;
+        }
+
+        System.arraycopy(arr, 0, newArr, 0, length_to_index);
+         
+        int length_to_end = 0;
+        //for loop to find the length to the rest of the array
+        for (int i = remove_index + 1; i < arr.length; i++){
+            length_to_end++;
+        }
+
+        System.arraycopy(arr, remove_index+1, newArr, length_to_index, length_to_end);
+        
+		return newArr;
+	}
+
+	//helper function to remove an element from an array but for ints
+	public int[] removeElement(int[] arr, int remove_index){
+		int[] newArr = new int[arr.length - 1];
+		int length_to_index = 0;
+        //for loop to find the length to the index to be removed
+        for (int i = 0; i < arr.length; i++) {
+            if (length_to_index == remove_index){
+                break;
+            }
+			length_to_index++;
+        }
+
+        System.arraycopy(arr, 0, newArr, 0, length_to_index);
+         
+        int length_to_end = 0;
+        //for loop to find the length to the rest of the array
+        for (int i = remove_index + 1; i < arr.length; i++){
+            length_to_end++;
+        }
+
+        System.arraycopy(arr, remove_index+1, newArr, length_to_index, length_to_end);
+        
+		return newArr;
+	}
+
+	public Polynomial add(Polynomial another_polynomial) {
+		//lengths of both polynomials
+		int length1;
+		int length2;
+		//only read length if it is not null
+		if (exponents != null && another_polynomial.exponents != null){
+			length1 = exponents.length;
+			length2 = another_polynomial.exponents.length;
+		}
+		//else one of them is null, return accordingly
+		else if (exponents == null){
+			return another_polynomial;
+		}
+		else if (another_polynomial.exponents == null){
+			return this;
+		}
+		else {
+			return null;
+		}
+
+		//variable to hold the biggest exponent, as that will help us get the length of the sum polynomial.
+		int biggest_exponent = 0;
+
+		//loop to find this
+		for (int i = 0; i < length1; i++) {
+			if (exponents[i] > biggest_exponent){
+				biggest_exponent = exponents[i];
+			}
+		}
+		//loop again for the second exponents array
+		for (int i = 0; i < length2; i++) {
+			if (another_polynomial.exponents[i] > biggest_exponent){
+				biggest_exponent = another_polynomial.exponents[i];
+			}
+		}
+
+		//the max length of the new polynomial is the biggest exponent +1.
+		int max_length = biggest_exponent + 1;
+
+		//arrays for the sum polynomial
+		double[] sum_coefficients = new double[max_length];
+		Arrays.fill(sum_coefficients, 0.0);
+		int[] sum_exponents = new int[max_length];
+		Arrays.fill(sum_exponents, 0);
+
+		//loops to add the exponents in
+		for (int i = 0; i < max_length; i++){
+			for(int j = 0; j < length1; j++){
+				if (i == exponents[j]) {
+					sum_exponents[i] = exponents[j];
+				}
+			}
+			for(int j = 0; j < length2; j++){
+				if (i == another_polynomial.exponents[j]) {
+					sum_exponents[i] = another_polynomial.exponents[j];
+				}
+			}
+		}
+
+		//loops to add coefficients in
+		for (int j = 0; j < length1; j++){
+			sum_coefficients[exponents[j]] += coefficients[j];
+			
+		}
+		for (int j = 0; j< length2; j++){
+			sum_coefficients[another_polynomial.exponents[j]] += another_polynomial.coefficients[j];
+		}
+
+		//for loops to remove redundant values
+		for (int i = 0; i < max_length; i++){
+			// System.out.println(Arrays.toString(sum_coefficients));
+			// System.out.println(Arrays.toString(sum_exponents));
+			for (int j = 0; j < max_length; j++){
+				if ((sum_exponents[j] == 0) && (sum_coefficients[j] == 0.0)){
+					int[] temp = sum_exponents.clone();
+					temp = removeElement(temp, j);
+					sum_exponents = temp.clone();
+				}
+				if (sum_coefficients[j] == 0.0){
+					sum_coefficients = removeElement(sum_coefficients, j);
+					max_length--;
+				}
+			}
+			//System.out.println(max_length);
+		}
+		// System.out.println(Arrays.toString(sum_coefficients));
+		// System.out.println(Arrays.toString(sum_exponents));
+
+		//construct the polynomial and return
+		Polynomial sum_Polynomial = new Polynomial(sum_coefficients, sum_exponents);
+		return sum_Polynomial;
 	}
 	
 	public double evaluate(double x_value) {
@@ -58,24 +239,152 @@ public class Polynomial {
 	public Polynomial multiply(Polynomial inputPolynomial) {
 		int length1 = coefficients.length;
 		int length2 = inputPolynomial.coefficients.length;
+
+		if ((length1 == 0) || (length2 == 0)){
+			Polynomial product_Polynomial = new Polynomial();
+			return product_Polynomial;
+		}
 		
-		//calculate total length of the product polynomial, which is length1 + length2 - 1 (I think, did some test cases)
-		int product_length = length1 + length2 - 1;
+		//calculate total length of the product polynomial (can remove redundancy later)
+		int product_length = length1 + length2 + 2;
 		
+		// //declare some arrays to be the sorted versions of our originals
+		// double[] caller_sorted_coefficients = coefficients.clone();
+		// int[] caller_sorted_exponents = exponents.clone();
+
+		// double[] input_sorted_coefficients = inputPolynomial.coefficients.clone();
+		// int[] input_sorted_exponents = inputPolynomial.exponents.clone();
+
+		// //sort the exponents, but not the coefficients 
+		// Arrays.sort(caller_sorted_exponents);
+		// Arrays.sort(input_sorted_exponents);
+
+		// //for loop to line the caller coefficients up with their exponents
+		// for (int i = 0; i < length1; i++){
+		// 	//ints to help sort
+		// 	int original_caller_exponent_index = 0;
+		// 	for(int j = 0; j < length1; j++){
+		// 		if (caller_sorted_exponents[i] == exponents[j]){
+		// 			original_caller_exponent_index = j;
+		// 		}
+		// 	}
+		// 	caller_sorted_coefficients[i] = coefficients[original_caller_exponent_index];
+		// }
+
+		// //for loop to line the input coefficients up with their exponents
+		// for (int i = 0; i < length2; i++){
+		// 	//ints to help sort
+		// 	int original_input_exponent_index = 0;
+		// 	for(int j = 0; j < length2; j++){
+		// 		if (input_sorted_exponents[i] == inputPolynomial.exponents[j]){
+		// 			original_input_exponent_index = j;
+		// 		}
+		// 	}
+		// 	input_sorted_coefficients[i] = inputPolynomial.coefficients[original_input_exponent_index];
+		// }
+
+		// System.out.println(Arrays.toString(caller_sorted_coefficients));
+		// System.out.println(Arrays.toString(caller_sorted_exponents));
+		// System.out.println(Arrays.toString(input_sorted_coefficients));
+		// System.out.println(Arrays.toString(input_sorted_exponents));
+
 		//declare empty arrays to use for the product polynomial
 		double[] product_coefficients = new double[product_length];
 		int[] product_exponents = new int[product_length];
-		
+
+		//make this into a polynomial ahead of time, so we can use add() method 
 		Polynomial product_Polynomial = new Polynomial(product_coefficients, product_exponents);
-		
+
 		//nested for loop to multiply polynomials, the length does not matter this time cuz we are multiplying
 		for (int i = 0; i < length1; i++){
 			for (int j = 0; j < length2; j++) {
-				product_coefficients[i + j] += coefficients[i]*inputPolynomial.coefficients[j];
-				product_exponents[i + j] = exponents[i] + inputPolynomial.exponents[j];
+				double[] current_value = {coefficients[i] * inputPolynomial.coefficients[j]};
+				int[] current_exponent = {exponents[i] + inputPolynomial.exponents[j]};
+				//new polynomial to add to product_Polynomial
+				Polynomial temp_Polynomial = new Polynomial(current_value, current_exponent);
+
+				product_Polynomial = product_Polynomial.add(temp_Polynomial);
 			}
 		}
 
 		return product_Polynomial;
+	}
+
+	public Polynomial(File filePolynomial) {
+    try {
+		Polynomial newPolynomial = new Polynomial();
+
+		//temp polynomial to add
+		Polynomial tempPolynomial = new Polynomial();
+		double[] temp_coefficients;
+		int[] temp_exponents;
+
+        BufferedReader reader = new BufferedReader(new FileReader(filePolynomial));
+        String polynomial_as_a_String = reader.readLine();
+		String[] polynomial_as_an_Array = polynomial_as_a_String.split("");
+		System.out.println(Arrays.toString(polynomial_as_an_Array));
+        for (int i = 0; i < polynomial_as_an_Array.length; i++){
+			if (isCoefficient(polynomial_as_an_Array, i)){
+				//check if its negative
+				if (i != 0 && polynomial_as_an_Array[i - 1].equals("-")){
+					temp_coefficients = new double[] {-Double.parseDouble(polynomial_as_an_Array[i])};
+				}
+				else{
+					temp_coefficients = new double[] {Double.parseDouble(polynomial_as_an_Array[i])};
+
+				}
+				//check if its a coefficient
+				if (isExponent(polynomial_as_an_Array, i + 2)){
+					temp_exponents = new int[] {Integer.parseInt(polynomial_as_an_Array[i + 2])};
+				}
+				//else it is a constant
+				else {
+					temp_exponents = new int[] {0};
+				}
+				tempPolynomial = new Polynomial(temp_coefficients, temp_exponents);
+				newPolynomial = newPolynomial.add(tempPolynomial);
+			}
+		}
+
+
+		coefficients = newPolynomial.coefficients.clone();
+		exponents = newPolynomial.exponents.clone();
+
+        reader.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found: " + filePolynomial.getAbsolutePath());
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Error reading the file: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public void saveToFile(String fileName){
+		try {
+			String polynomialString = "";
+			//I will convert stuff to int cuz it looks better :)
+			for (int i = 0; i < coefficients.length; i++){
+				//if the coefficient is not the first one, we need a plus sign.
+				if (coefficients[i] > 0.0 && i != 0){
+					polynomialString = polynomialString + "+" + (int)coefficients[i];
+				}
+				//if its negative, the negative sign is added in with the double. if it is the first one, then no need for + sign.
+				else {
+					polynomialString = polynomialString + (int)coefficients[i];
+				}
+				//then do exponent, but only put down an x if the exponent is nonzero.
+				if (exponents[i] != 0){
+					polynomialString = polynomialString + "x" + exponents[i];
+				}
+			}
+			FileWriter writer = new FileWriter(fileName);
+			writer.write(polynomialString);
+			writer.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("alright clearly you didn't write to the file properly.");
+		}
+
 	}
 }
